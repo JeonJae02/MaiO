b6cd1952cc09b2df75d4ba311267070394082c79
root <root@DESKTOP-ADKH6AP.localdomain>
2025-07-28 03:54:38 +0900

    fetcher silsu

--- Maio_Web_bepo/src/utils/fetcher.ts
+++ Maio_Web_bepo/src/utils/fetcher.ts
@@ -1,4 +1,4 @@
-export const API_BASE_URL = 'http://127.0.0.1:5000/api'; // API 기본 URL
+export const API_BASE_URL = '/api'; // API 기본 URL
 
 export async function fetchJson<T>(url: string, options?: RequestInit): Promise<T> {
   const response = await fetch(url, options);
